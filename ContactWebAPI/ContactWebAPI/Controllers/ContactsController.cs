cda0ba6114b45ce187fbb59a77c819b85460796e
Brian Redmond <brian.redmond@live.com>
2015-08-26 15:18:09 -0400

    Added Bird

--- ContactWebAPI/ContactWebAPI/Controllers/ContactsController.cs
+++ ContactWebAPI/ContactWebAPI/Controllers/ContactsController.cs
@@ -17,7 +17,7 @@ namespace ContactsList.Controllers
             return new Contact[]{
                 new Contact { Id = 1, EmailAddress = "airjordan@contoso.com", Name = "Michael Jordan"},
                 new Contact { Id = 2, EmailAddress = "brianredmond@contoso.com", Name = "Brian Redmond"},
-                new Contact { Id = 3, EmailAddress = "magicjohnson@contoso.com", Name = "Magic"},
+                new Contact { Id = 3, EmailAddress = "bird@contoso.com", Name = "Larry Bird"},
                 new Contact { Id = 4, EmailAddress = "lebron@contoso.com", Name = "LeBron James"}
             };
         }
